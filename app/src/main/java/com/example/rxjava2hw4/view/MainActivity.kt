0b02e2fe0698ff8e8f8821ff43c9e771580e4ca0
Stepan <bai_777@mail.ru>
2021-12-15 18:45:47 +0300

    создал структуру папок mvp

--- app/src/main/java/com/example/rxjava2hw4/view/MainActivity.kt
+++ app/src/main/java/com/example/rxjava2hw4/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.rxjava2hw4
+package com.example.rxjava2hw4.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rxjava2hw4.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
